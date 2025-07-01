b9d4b8aa358b800fc5b126d508066c0f2cebd4ff
Vladimir Drugov <wolffdrugov@yandex.ru>
2025-07-01 22:39:43 +0300

    master:updated users table

--- app/Service/Gambling/GamblingMessage.php
+++ app/Service/Gambling/GamblingMessage.php
@@ -40,7 +40,7 @@ class GamblingMessage
     public function getMostWinsByCount(): object
     {
         $baseQuery = \App\Models\GamblingMessage::with('user')
-            ->select('user_id', DB::raw('COUNT(*) as win_count'))
+            ->select('user_id', DB::raw('COUNT(*) as win_count'), 'user.name')
             ->groupBy('user_id')
             ->orderByDesc('win_count');
 
@@ -59,21 +59,37 @@ class GamblingMessage
         ];
 
         foreach ($topWinners as $topWinner) {
+
             $userWinCount = $totalUsersTriesCount[$topWinner->user_id];
+            $userPercentStats = (object)[
+                'name'           => $topWinner->user->name,
+                'userWinPercent' => 0,
+            ];
+            $userCountStats = (object)[
+                'userWinCount'   => $userWinCount,
+                'name'           => $topWinner->user->name,
+            ];
             if ($userWinCount != 0) {
-                $result->win_percent[$topWinner->user_id] = $topWinner->win_count
-                    / $totalUsersTriesCount[$topWinner->user_id];
-                $result->win_count[$topWinner->user_id] = $topWinner->win_count;
+                $userWinPercent = ($topWinner->win_count
+                        / $totalUsersTriesCount[$topWinner->user_id]) * 100;
+                $userPercentStats->userWinPercent = $userWinPercent;
+                $result->win_percent[$topWinner->user_id] = $userPercentStats;
+                $result->win_count[$topWinner->user_id] = $userCountStats;
             }
         }
-        return $topWinners;
+        return $result;
     }
 
     public function getMostWinsByMoney(): Collection
     {
         $topWinners = \App\Models\GamblingMessage::with('user')
             ->where('is_win', '=', true)
-            ->select('user_id', DB::raw('COUNT(*) as win_count'), DB::raw('SUM(win_price) as win_sum'))
+            ->select(
+                'user_id',
+                DB::raw('COUNT(*) as win_count'),
+                DB::raw('SUM(win_price) as win_sum'),
+                'user.name'
+            )
             ->groupBy('user_id')
             ->orderByDesc('win_sum')
             ->limit(3)
